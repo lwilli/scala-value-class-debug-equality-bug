aef2368f71af13cb9614a3678c753dd298ea165c
logan.williams <logan.williams@workday.com>
2022-01-10 14:59:00 -0800

    Simplify by removing unnecessary type alias

--- src/main/scala/BugDemo.scala
+++ src/main/scala/BugDemo.scala
@@ -7,26 +7,17 @@
  */
 object BugDemo extends App {
   class Value(val underlying: String) extends AnyVal
-  type ValueAlias = Value
 
   // It doesn't matter how we instantiate this val or what type we annotate it with--all combinations show the same behavior.
   val value = new Value("same")
-  // The debugger evaluates these equality checks incorrectly
+
+  // The debugger evaluates this equality check incorrectly
   println(value == value)
-  println((value: Value) == value)
-  println(value == (value: Value))
-  println((value: Value) == (value: Value))
-  println((value: ValueAlias) == value)
-  println(value == (value: ValueAlias))
-  println((value: ValueAlias) == (value: ValueAlias))
-  
+
   // Interestingly, the debugger evaluates these correctly...
   println(new Value("same") == new Value("same"))
-  println((new Value("same"): ValueAlias) == new Value("same"))
+  println((new Value("same"): Value) == new Value("same"))
   // ...but not these
-  println(new Value("same") == (new Value("same"): ValueAlias))
-  println((new Value("same"): ValueAlias) == (new Value("same"): ValueAlias))
-  println((new Value("same"): Value) == (new Value("same"): ValueAlias))
-  println((new Value("same"): ValueAlias) == (new Value("same"): Value))
+  println(new Value("same") == (new Value("same"): Value))
   println((new Value("same"): Value) == (new Value("same"): Value))
 }
